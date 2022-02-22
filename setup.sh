ef09277c4703b543e91e5896e4a98145ae716476
Paul Abrams <john.paul.abrams@gmail.com>
2022-02-22 05:55:37 -0500

    feat: init

---
+++ setup.sh
@@ -0,0 +1,41 @@
+
+# tilde
+sudo apt-get install tilde
+
+# guake
+sudo apt-get install guake
+
+# load guake preferences
+guake --restore-preferences ./guake_prefs
+
+# VS Code
+sudo apt-get install code
+code --install-extension paulabrams1.trade-wars-2002-color-theme
+
+# npm
+sudo apt install npmchore: increment version
+
+# Joplin
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
+
+# chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
+
+# Midnight Commander
+sudo apt install mc
+
+# duck-duck-go cli
+sudo apt install ddgr
+
+# snap packagemanaer 
+sudo apt install snapd
+
+# btop, neofetch sysinfo
+snap install btop
+sudo apt install neofetch
+
+# git config
+git config --global user.email "john.paul.abrams@gmail.com"
+git config --global user.name "Paul Abrams"
