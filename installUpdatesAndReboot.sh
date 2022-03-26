7493dc36a3f4accecff68c4639f268f34bfb265f
Paul Abrams <john.paul.abrams@gmail.com>
2022-03-26 16:09:38 -0400

    add update commands

---
+++ installUpdatesAndReboot.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo apt autoremove
+sudo apt autoclean
+sudo fwupdmgr get-devices
+sudo fwupdmgr get-updates
+sudo fwupdmgr update
+flatpak update
+sudo reboot now
