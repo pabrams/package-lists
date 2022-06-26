b5dc79a53715eb1bd402cfc03e21aaba2a7ce0f7
Paul Abrams <john.paul.abrams@gmail.com>
2022-06-26 01:54:32 -0400

    use verbose parameters in export pkg commands

--- EndeavourOS/export-packages.sh
+++ EndeavourOS/export-packages.sh
@@ -1,2 +1,3 @@
-sudo pacman -Qqe > ./EndeavourOS/pacman-Qqe.txt
-pacman -Qe >  ./EndeavourOS/pacman-Qe.txt
+sudo pacman --query --quiet --explicit > ./EndeavourOS/pacman-quiet-explicit.txt
+pacman --query --explicit >  ./EndeavourOS/pacman-explicit.txt
+pacman --query --info --explicit > ./EndeavourOS/pacman-info-explicit.txt
