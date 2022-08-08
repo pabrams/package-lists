b5d899ed6f6b9fa13497816406a3c5092f2ae7f5
Paul Abrams <john.paul.abrams@gmail.com>
2022-08-08 02:12:09 -0400

    export-packages: use running script dir instead of incorrect rel path

--- EndeavourOS/export-packages.sh
+++ EndeavourOS/export-packages.sh
@@ -1,3 +1,3 @@
-sudo pacman --query --quiet --explicit > ./EndeavourOS/pacman-quiet-explicit.txt
-pacman --query --explicit >  ./EndeavourOS/pacman-explicit.txt
-pacman --query --info --explicit > ./EndeavourOS/pacman-info-explicit.txt
+sudo pacman --query --quiet --explicit > $(dirname "$0")/pacman-quiet-explicit.txt
+pacman --query --explicit >  $(dirname "$0")/pacman-explicit.txt
+pacman --query --info --explicit > $(dirname "$0")/pacman-info-explicit.txt
