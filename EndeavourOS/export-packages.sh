306aec6bbcb4c49f5077753e878518a6c073f6c5
Paul Abrams <john.paul.abrams@gmail.com>
2022-08-08 02:23:49 -0400

    export-packages: use $BASH_SOURCE, not $0

--- EndeavourOS/export-packages.sh
+++ EndeavourOS/export-packages.sh
@@ -1,3 +1,3 @@
-sudo pacman --query --quiet --explicit > $(dirname "$0")/pacman-quiet-explicit.txt
-pacman --query --explicit >  $(dirname "$0")/pacman-explicit.txt
-pacman --query --info --explicit > $(dirname "$0")/pacman-info-explicit.txt
+sudo pacman --query --quiet --explicit > $(dirname "$BASH_SOURCE")/pacman-quiet-explicit.txt
+pacman --query --explicit >  $(dirname "$BASH_SOURCE")/pacman-explicit.txt
+pacman --query --info --explicit > $(dirname "$BASH_SOURCE")/pacman-info-explicit.txt
